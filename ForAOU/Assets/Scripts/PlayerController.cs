868bf7d5fe7f58ca70d4878f154129c3af106ef4
KimYoungJun <kyj931221@gmail.com>
2024-06-20 00:46:39 +0900

    플레이어 이동 구현

---
+++ ForAOU/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioClip deathClip;
+    public float jumpForce = 700f;
+    public float maxSpeed;
+
+    private int jumpCount = 0;
+    private bool isGrounded = false;
+    private bool isDead = false;
+
+    private Rigidbody2D playerRigidbody;
+    private Animator animator;
+    private AudioSource playerAudio;
+    private SpriteRenderer PlayerSpriteRenderer;
+   
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        PlayerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+   
+    void Update()
+    {
+        if(isDead) return;
+
+        if(Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            jumpCount++;
+            playerRigidbody.velocity = Vector2.zero;
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            playerAudio.Play();
+        }
+        else if(Input.GetButtonUp("Jump") && playerRigidbody.velocity.y > 0) 
+        {
+            playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+        }
+        animator.SetBool("Grounded",isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        playerRigidbody.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if(playerRigidbody.velocity.x > maxSpeed)
+        {
+            playerRigidbody.velocity = new Vector2(maxSpeed, playerRigidbody.velocity.y);
+            PlayerSpriteRenderer.flipX = false;
+        }
+        else if(playerRigidbody.velocity.x < maxSpeed*(-1))
+        {
+            playerRigidbody.velocity = new Vector2(maxSpeed*(-1), playerRigidbody.velocity.y);
+            PlayerSpriteRenderer.flipX = true;
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Die");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerRigidbody.velocity = Vector2.zero;
+        isDead = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
+
+        if(other.tag == "Goal" && !isDead)
+        {
+            // 플레이어가 목표지점에 도달했을 때 이벤트 발생.
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
